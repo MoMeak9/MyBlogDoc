f8424a2ea3c034e23c940c0734f55ae206b0c95a
MoMeak9 <1308994506@qq.com>
2022-04-22 09:29:16 +0800

    feat(base)：baseURL更新

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineHopeConfig } from "vuepress-theme-hope";
 import themeConfig from "./themeConfig";
 
 export default defineHopeConfig({
-  base: "/MyBlogDoc/",
+  base: "/",
 
   dest: "./dist",
 
