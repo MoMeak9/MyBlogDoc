b61af08d4451bebf5542cff95dc47823a5d96d5d
MoMeak9 <shi.yihui@iwhalecloud.com>
2022-11-19 19:08:10 +0800

    docs: ✏️ B站面经

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -38,44 +38,13 @@ export default hopeTheme({
     pageInfo: ["Author", "Original", "Date", "Category", "Tag", "ReadingTime"],
 
     blog: {
-        description: "前端技术狂热者",
+        description: "泯泷 - 前端开发爱好者",
         intro: "/intro.html",
         medias: {
-            // Baidu: "https://example.com",
-            // Bitbucket: "https://example.com",
-            // Dingding: "https://example.com",
-            // Discord: "https://example.com",
-            // Dribbble: "https://example.com",
+            // https://vuepress-theme-hope.github.io/v2/zh/config/theme/feature.html#blog-medias
             Email: "mailto:shi_yihui@qq.com",
-            // Evernote: "https://example.com",
-            // Facebook: "https://example.com",
-            // Flipboard: "https://example.com",
-            // Gitee: "https://gitee.com/shiyifei332",
             GitHub: "https://github.com/MoMeak9",
-            // Gitlab: "https://example.com",
-            // Gmail: "mailto:shi_yihui@qq.com",
-            // Instagram: "https://example.com",
-            // Lines: "https://example.com",
-            // Linkedin: "https://example.com",
-            // Pinterest: "https://example.com",
-            // Pocket: "https://example.com",
-            // QQ: "https://example.com",
-            // Qzone: "https://example.com",
-            // Reddit: "https://example.com",
-            // Rss: "https://example.com",
-            // Steam: "https://example.com",
-            // Twitter: "https://example.com",
-            // Wechat: "https://example.com",
-            // Weibo: "https://example.com",
-            // Whatsapp: "https://example.com",
-            // Youtube: "https://example.com",
-            // Zhihu: "https://example.com",
-        },
-    },
-
-    encrypt: {
-        config: {
-            "/guide/encrypt.html": ["1234"],
+            BiliBili: "https://space.bilibili.com/1000000000",
         },
     },
 
