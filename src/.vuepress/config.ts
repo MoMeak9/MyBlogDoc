3f8fc1c2cf043055d19b19b106aa86bb5ceba80a
MoMeak9 <1308994506@qq.com>
2023-07-11 01:40:05 +0800

    feat: 🎸 SEO 标签

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,6 +1,6 @@
 import {defineUserConfig} from "vuepress";
 import {searchProPlugin} from "vuepress-plugin-search-pro";
-import { cut } from "nodejs-jieba";
+import {cut} from "nodejs-jieba";
 import theme from "./theme";
 
 export default defineUserConfig({
@@ -20,12 +20,20 @@ export default defineUserConfig({
             s.parentNode.insertBefore(hm, s);
             })();`
         ],
-        [
-            'meta', {name: 'google-site-verification', content: '9vkxYqOlDYYQz21WsvuXk9ztRIK5L0_MuzL1WvFmBuY'}
-        ],
-        [
-            'meta', {name: 'keywords', content: '前端博客 vuepress Vue React JavaScript'}
-        ]
+        ['meta', {name: 'google-site-verification', content: '9vkxYqOlDYYQz21WsvuXk9ztRIK5L0_MuzL1WvFmBuY'}],
+        // 关键词
+        ['meta', {name: 'keywords', content: '前端博客, vuepress, Vue, React, JavaScript'}],
+        ['meta', {charset: 'utf-8'}],
+        ['meta', {name: 'viewport', content: 'width=device-width, initial-scale=1.0'}],
+        ['meta', {name: 'author', content: '泯泷 Yihui'}],
+        // 网站图标
+        ['link', {rel: 'icon', href: '/favicon.ico'}],
+        // Open Graph (OG) 标签
+        ['meta', {property: 'og:type', content: 'website'}],
+        ['meta', {property: 'og:url', content: 'https://yihuiblog.top/'}],
+        ['meta', {property: 'og:title', content: 'Yihui\'s Blog'}],
+        ['meta', {property: 'og:description', content: '前端小白的技术博客'}],
+        ['meta', {property: 'og:image', content: 'https://cdn.yihuiblog.top/images/logo-for-blog.jpg'}],
     ],
 
     locales: {
